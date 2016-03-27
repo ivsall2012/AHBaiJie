3de34b4b50f231727b36086a70cba4ec5b8973b0
Hurricane <ivsall2012@gmail.com>
2016-03-27 13:04:29 -0700

    initial setup

--- BaiJie/AppDelegate.m
+++ BaiJie/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,12 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
+    ViewController *vc = [[ViewController alloc]init];
+    vc.view.backgroundColor = [UIColor redColor];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
