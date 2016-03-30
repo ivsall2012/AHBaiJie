463a9f2c1fc35d043c8c8ee73b3bb9ded97c7838
Hurricane <ivsall2012@gmail.com>
2016-03-30 12:35:11 -0700

    TabBar color bug fix

--- BaiJie/AHTabBarVC.m
+++ BaiJie/AHTabBarVC.m
@@ -16,9 +16,9 @@
 @implementation AHTabBarVC
 +(void)initialize{
     UITabBarItem *barItem = [UITabBarItem appearance];
-    [barItem setTitleTextAttributes:@{NSForegroundColorAttributeName:AHColor(140, 132, 128)} forState:UIControlStateNormal];
+    // you have to set normal or selected states all at once, otherwise it only works on the latest settings.
+    [barItem setTitleTextAttributes:@{NSForegroundColorAttributeName:AHColor(140, 132, 128),NSFontAttributeName:AHTabBarFont} forState:UIControlStateNormal];
     [barItem setTitleTextAttributes:@{NSForegroundColorAttributeName:AHColor(81, 81, 81)} forState:UIControlStateSelected];
-    [barItem setTitleTextAttributes:@{NSFontAttributeName:AHTabBarFont} forState:UIControlStateNormal];
 }
 
 -(void)viewDidLoad{
